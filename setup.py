2b46553b879ff58c8cb970ec34b24314162317a4
Ben Heiskell <bheiskell@factset.com>
2013-03-31 15:07:41 -0400

    Added flask sql dependency. Added a comment and pylint suppression as well.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+"""Setup script for Logolas."""
 from setuptools import setup
 
 # http://stackoverflow.com/questions/9352656/python-assertionerror-when-running-nose-tests-with-coverage
-from multiprocessing import util
+from multiprocessing import util #pylint: disable=W0611
 
 setup(
     name = "Logolas",
@@ -27,6 +28,7 @@ setup(
     install_requires=[
         'flask==0.9',
         'sqlalchemy==0.8.0',
+        'Flask-SQLAlchemy==0.16',
         'pyinotify==0.9.4',
         'pyyaml==3.10',
     ],
