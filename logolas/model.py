14d530c6215a6c3c347bf0539bd399bfc288664e
Ben Heiskell <bheiskell@factset.com>
2013-03-31 16:38:27 -0400

    Adding tests for the model file.

--- logolas/model.py
+++ logolas/model.py
@@ -2,16 +2,20 @@
 
 from collections import OrderedDict
 from sqlalchemy import Table, Column, Integer, DateTime, String
-
 from sqlalchemy.orm import mapper
 import logging
 
 _LOG = logging.getLogger(__name__)
 
+IGNORED_COLUMNS=['id', 'time', 'date']
+
 def get_table(metadata, name, fields, order):
     """
-    Dynamically generate the Table and model class for a give Table and Fields.
-    The schema will be added to the sqlalchemy.MetaData instance provided.
+    Dynamically create the table and model class.
+
+    * The table will by default have an auto-incrementing id and time datetime.
+    * The id, date, and time in both fields and order are ignored.
+    * The generated table is automatically added to the metadata and mapped to the model class.
     """
 
     table = Table(
@@ -22,26 +26,24 @@ def get_table(metadata, name, fields, order):
     )
 
     for field in fields:
-        if field not in ['date', 'time']:
+        if field not in IGNORED_COLUMNS:
             table.append_column(Column(field, String))
 
     class Model(object): #pylint: disable=R0903
-        """
-        There's probably a better way to do this, but I need a model class for
-        this table. This allows me to instantiate an ORM instance and add it to
-        a SqlAlchemy session.
-        """
+        """This table's model class."""
+
         def __init__(self, entry):
-            self.entry = entry
+            # enable the dynamic access to fields via accessors
             for field, value in entry.items():
                 self.__dict__[field] = value
 
         def get_data(self):
             """Return 'data' components in an ordered dictionary"""
+
             result = OrderedDict()
 
             for field in order:
-                if field not in ['id', 'time', 'date']:
+                if field not in IGNORED_COLUMNS:
                     result[field] = getattr(self, field)
 
             return result
--- logolas/tests/test_models.py
+++ logolas/tests/test_models.py
@@ -1,7 +1,93 @@
+"""Test the dynamic creation of models."""
+
+from collections import deque
+from datetime import datetime
+from logolas.model import get_table
+from sqlalchemy import MetaData, DateTime, Integer
+from sqlalchemy.exc import InvalidRequestError
 from unittest import TestCase
 
-import logolas
+#pylint: disable=R0904
 
 class TestModels(TestCase):
-    def test_is_string(self):
-        self.assertTrue(True)
+    """Test the dynamic creation of models."""
+
+    def setUp(self): #pylint: disable=C0103
+        """Initialize"""
+        self.metadata = MetaData()
+        self.fields = [ 'time', 'test1', 'test2' ]
+        self.order = [ 'test2', 'test1' ]
+        (self.table, self.model_class) = get_table(self.metadata, 'table', self.fields, self.order)
+
+    def test_construction(self):
+        """Initialize"""
+
+        self.assertEquals('table', self.table.name)
+
+        column_names = [ column.name for column in self.table.columns ]
+        for field in self.fields:
+            self.assertIn(field, column_names)
+
+    def test_model(self):
+        """Initialize"""
+
+        model = self.model_class({'id': 1, 'time': datetime.now(), 'test1': 'test1', 'test2': 'test2'})
+
+        #pylint: disable=E1101
+        self.assertEquals('test1', model.test1)
+        self.assertEquals('test2', model.test2)
+        self.assertEquals(1, model.id)
+
+    def test_order(self):
+        """Initialize"""
+
+        model = self.model_class({'id': 1, 'time': datetime.now(), 'test1': 'test1', 'test2': 'test2'})
+
+        order = deque(self.order)
+        for key in model.get_data().keys():
+            self.assertEquals(order.popleft(), key)
+
+        self.assertEquals(0, len(order))
+
+    def test_difference(self):
+        """Test that two tables don't return the same instances."""
+
+        (table, model_class) = get_table(self.metadata, 'table_2', self.fields, self.order)
+
+        self.assertNotEqual(self.table, table)
+        self.assertNotEqual(self.model_class, model_class)
+
+    def test_duplicate(self):
+        """Test that the same table cannot be created twice even with different columns."""
+
+        self.assertRaises(InvalidRequestError, get_table, self.metadata, 'table', {}, self.order)
+
+    def test_field_ignoring(self):
+        """Assert special fields are ignored."""
+
+        self.fields.append('id')
+        self.fields.append('date')
+        self.fields.append('time')
+
+        (table, model_class) = get_table(self.metadata, 'table_2', self.fields, self.order) #pylint: disable=W0612
+
+        for column in table.columns:
+            if column.name == 'id':
+                self.assertEqual(type(column.type), Integer)
+            elif column.name == 'time':
+                self.assertEqual(type(column.type), DateTime)
+            self.assertNotEqual('date', column.name)
+
+    def test_order_ignoring(self):
+        """Assert special orders are ignored."""
+
+        self.order.append('id')
+        self.order.append('date')
+        self.order.append('time')
+
+        (table, model_class) = get_table(self.metadata, 'table_2', self.fields, self.order) #pylint: disable=W0612
+
+        model = model_class({'id': 1, 'time': datetime.now(), 'test1': 'test1', 'test2': 'test2'})
+
+        for key in model.get_data().keys():
+            self.assertNotIn(key, ['id', 'date', 'time'])
